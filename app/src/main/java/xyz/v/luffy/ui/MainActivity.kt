be9a894b558a129c9c4f47001548a07385c93ae0
Venu <tooblespprt@gmail.com>
2021-09-24 10:52:52 +0530

    ui updates - microinteractions

--- app/src/main/java/xyz/v/luffy/ui/MainActivity.kt
+++ app/src/main/java/xyz/v/luffy/ui/MainActivity.kt
@@ -1,25 +1,23 @@
 package xyz.v.luffy.ui
 
-import Base
 import android.animation.Animator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.Vibrator
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.view.ViewAnimationUtils
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -34,6 +32,22 @@ import xyz.v.luffy.models.Chat
 import xyz.v.luffy.viewmodel.MessageViewModel
 import java.util.*
 import kotlin.collections.ArrayList
+import Base
+import android.animation.StateListAnimator
+import android.os.*
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.widget.*
+import androidx.dynamicanimation.animation.SpringForce
+import androidx.transition.TransitionManager
+import app.rive.runtime.kotlin.RiveAnimationView
+import app.rive.runtime.kotlin.core.Fit
+import app.rive.runtime.kotlin.core.Loop
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.transition.MaterialArcMotion
+import com.google.android.material.transition.MaterialContainerTransform
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -44,15 +58,35 @@ class MainActivity : AppCompatActivity() {
     val sr = StringReplies
     var isIncognito = false
     var toolbar:RelativeLayout? = null
+    lateinit var iv:RiveAnimationView
+    lateinit var close:ImageView
+    lateinit var speechRecognizer:SpeechRecognizer
+    lateinit var box:RelativeLayout
+    lateinit var boxcont:RelativeLayout
+    lateinit var fab:FloatingActionButton
+    lateinit var stttv:TextView
+    lateinit var rive:RiveAnimationView
+    lateinit var vm:MessageViewModel
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val sendBtn:ImageView = findViewById(R.id.send)
         val editText:EditText = findViewById(R.id.edit_query)
-        val vm:MessageViewModel = ViewModelProvider(this).get(MessageViewModel::class.java)
+        vm = ViewModelProvider(this).get(MessageViewModel::class.java)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        box = findViewById(R.id.cardFab)
+        boxcont = findViewById(R.id.boxcont)
+        fab = findViewById(R.id.fab)
         toolbar = findViewById(R.id.toolbar)
         typing = findViewById(R.id.typing)
+        stttv = findViewById(R.id.stt)
+        iv = findViewById(R.id.icon)
+        close = findViewById(R.id.close)
+        rive = findViewById(R.id.rive)
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        init()
+
         window.statusBarColor = ContextCompat.getColor(this,R.color.colorPrimaryDarker)
         val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(
             this,
@@ -63,6 +97,12 @@ class MainActivity : AppCompatActivity() {
         vm.sendMessage().observe(this, {
             handleMessage(it)
         })
+
+
+
+        iv.setOnClickListener {
+            iv.play(Loop.ONESHOT)
+        }
 
         typing?.visibility = View.VISIBLE
         sendBtn.isEnabled = false
@@ -97,6 +137,154 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+        fab.setOnClickListener {
+            setUpSpring()
+        }
+
+        close.setOnClickListener {
+            restorefab(it)
+        }
+        keyboard.setOnClickListener {
+            boxcont.visibility = View.VISIBLE
+            it.visibility = View.GONE
+
+            SpringAnimation(fab, DynamicAnimation.TRANSLATION_Y, -150f).start()
+
+
+        }
+
+
+    }
+
+    fun restorefab(view:View) {
+
+        iv.reset()
+        speechRecognizer.stopListening()
+        speechRecognizer.cancel()
+
+        boxcont.visibility = View.GONE
+        keyboard.visibility = View.VISIBLE
+
+        val transform = MaterialContainerTransform().apply {
+            // Manually tell the container transform which Views to transform between.
+            startView = box
+            endView = fab
+
+            // Ensure the container transform only runs on a single target
+            addTarget(fab)
+            scrimColor = Color.TRANSPARENT
+
+            // Since View to View transforms often are not transforming into full screens,
+
+        }
+        transform.setPathMotion(MaterialArcMotion())
+
+        TransitionManager.beginDelayedTransition(fab.parent as ViewGroup, transform)
+        fab.visibility = View.VISIBLE
+        box.visibility = View.GONE
+
+
+        SpringAnimation(fab, DynamicAnimation.TRANSLATION_Y, 0f).start()
+
+    }
+
+    private fun init() {
+
+        speechRecognizer.setRecognitionListener(object : RecognitionListener{
+            override fun onReadyForSpeech(params: Bundle?) {
+
+            }
+
+            override fun onBeginningOfSpeech() {
+
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+
+            }
+
+            override fun onEndOfSpeech() {
+
+            }
+
+            override fun onError(error: Int) {
+
+            }
+
+            override fun onResults(results: Bundle?) {
+                val data: java.util.ArrayList<String>? =
+                    results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                val tt = data?.get(0).toString()
+                stttv.text = tt
+                Handler(Looper.getMainLooper()).postDelayed({
+                    rive.setRiveResource(R.raw.complt)
+                    rive.play(loop = Loop.ONESHOT)
+                    rive.fit = Fit.FILL
+                    val chat = Chat(1, tt)
+                    chatList.add(chat)
+                    adapter.notifyDataSetChanged()
+                    vm.setmessage(tt)
+                },2000)
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+
+
+
+
+    private fun setUpSpring() {
+
+        iv.play("Animation 2")
+
+
+        val transform = MaterialContainerTransform().apply {
+            // Manually tell the container transform which Views to transform between.
+            startView = fab
+            endView = box
+
+            // Ensure the container transform only runs on a single target
+            addTarget(box)
+            scrimColor = Color.TRANSPARENT
+
+            // Since View to View transforms often are not transforming into full screens,
+
+        }
+
+        stttv.text = "Listening..."
+        rive.setRiveResource(R.raw.listening)
+        rive.play(Loop.LOOP)
+        rive.fit = Fit.FILL
+
+        transform.setPathMotion(MaterialArcMotion())
+        transform.duration = 300
+        transform.containerColor = Color.WHITE
+        transform.startContainerColor = Color.WHITE
+        transform.endContainerColor = Color.WHITE
+        transform.setAllContainerColors(Color.WHITE)
+        val intentt = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intentt.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intentt.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        SpringAnimation(fab, DynamicAnimation.TRANSLATION_Y, -300f).addEndListener { animation, canceled, value, velocity ->
+            TransitionManager.beginDelayedTransition(fab.parent as ViewGroup, transform)
+            fab.visibility = View.GONE
+            box.visibility = View.VISIBLE
+            speechRecognizer.startListening(intentt)
+        }.start()
     }
 
     private fun handleMessage(base: Base) {
@@ -122,7 +310,6 @@ class MainActivity : AppCompatActivity() {
                     }else{
                         publicmode()
                         sendfromBot(sr.INCOGNITO_MODE_OFF)
-                        reveal()
                     }
                 }
                 "get_time" ->{
@@ -135,12 +322,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun sendfromBot(messg: String) {
         c++
         runOnUiThread {
-            if (c<2){
-                reveal()
-            }
             typing?.visibility = View.GONE
             val chat = Chat(2, messg)
             val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
@@ -150,16 +335,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun launchApp(packageName: String) {
-        // Get an instance of PackageManager
         val pm = applicationContext.packageManager
-
-        // Initialize a new Intent
         val intent:Intent? = pm.getLaunchIntentForPackage(packageName)
-
-        // Add category to intent
         intent?.addCategory(Intent.CATEGORY_LAUNCHER)
-
-        // If intent is not null then launch the app
         if(intent!=null){
             applicationContext.startActivity(intent)
         }else{
@@ -209,25 +387,25 @@ class MainActivity : AppCompatActivity() {
         val TAG = "MAin"
     }
 
-    fun reveal(){
-        val view:View = findViewById(R.id.bgCircle)
-        val view1:View = findViewById(R.id.bgCircleInc)
-        view1.visibility = View.GONE
-        val cy = view.height/2
-        val cx = view.width/2
-        val finalrad: Double = Math.hypot(cx.toDouble(), cy.toDouble())
-        val anim:Animator = ViewAnimationUtils.createCircularReveal(view,cx,cy, 0F,
-            finalrad.toFloat()
-        )
-        val vib:Vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            vib.vibrate(VibrationEffect.createOneShot(20,VibrationEffect.DEFAULT_AMPLITUDE))
-        }else{
-            vib.vibrate(20)
-        }
-        view.visibility = View.VISIBLE
-        anim.start()
-    }
+//    fun reveal(){
+//        val view:View = findViewById(R.id.bgCircle)
+//        val view1:View = findViewById(R.id.bgCircleInc)
+//        view1.visibility = View.GONE
+//        val cy = view.height/2
+//        val cx = view.width/2
+//        val finalrad: Double = Math.hypot(cx.toDouble(), cy.toDouble())
+//        val anim:Animator = ViewAnimationUtils.createCircularReveal(view,cx,cy, 0F,
+//            finalrad.toFloat()
+//        )
+//        val vib:Vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            vib.vibrate(VibrationEffect.createOneShot(20,VibrationEffect.DEFAULT_AMPLITUDE))
+//        }else{
+//            vib.vibrate(20)
+//        }
+//        view.visibility = View.VISIBLE
+//        anim.start()
+//    }
 
     fun revealIcognito(){
         val view:View = findViewById(R.id.bgCircleInc)
@@ -263,5 +441,11 @@ class MainActivity : AppCompatActivity() {
         chatList.add(chat)
         adapter.notifyDataSetChanged()
         recyclerView.scrollToPosition(chatList.lastIndex)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        speechRecognizer.destroy()
     }
 }
